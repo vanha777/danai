0cdc27d8a926b53d7d632013d4f9c56cc3413b4c
vanha777 <vanha777@icloud.com>
2024-09-12 01:49:55 +1000

    set up

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
+import DashBoard from "./view/dashBoard";
 
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
@@ -13,40 +14,83 @@ function App() {
   }
 
   return (
-    <div className="container">
-      <h1>Welcome to Tauri!</h1>
+    // <div className="container">
+    //   <h1>Welcome to Tauri!</h1>
 
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    //   <div className="row">
+    //     <a href="https://vitejs.dev" target="_blank">
+    //       <img src="/vite.svg" className="logo vite" alt="Vite logo" />
+    //     </a>
+    //     <a href="https://tauri.app" target="_blank">
+    //       <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
+    //     </a>
+    //     <a href="https://reactjs.org" target="_blank">
+    //       <img src={reactLogo} className="logo react" alt="React logo" />
+    //     </a>
+    //   </div>
+
+    //   <p>Click on the Tauri, Vite, and React logos to learn more.</p>
+
+    //   <form
+    //     className="row"
+    //     onSubmit={(e) => {
+    //       e.preventDefault();
+    //       greet();
+    //     }}
+    //   >
+    //     <input
+    //       id="greet-input"
+    //       onChange={(e) => setName(e.currentTarget.value)}
+    //       placeholder="Enter a name..."
+    //     />
+    //     <button type="submit">Greet</button>
+    //   </form>
+
+    //   <p>{greetMsg}</p>
+    // </div>
+
+    <main>
+
+      {/* <Sidebar /> */}
+      <div className="drawer">
+        <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content flex flex-col">
+          {/* Navbar */}
+          <div className="w-full navbar">
+            <div className="flex-none lg:hidden fixed">
+              <label htmlFor="my-drawer-3" aria-label="open sidebar" className="btn btn-square btn-ghost">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+                </svg>
+              </label>
+            </div>
+            {/* <div className="flex-1 px-2 mx-2">Navbar Title</div> */}
+            <div className="flex-none hidden lg:block">
+              <ul className="menu menu-horizontal">
+                {/* Navbar menu content here */}
+                <li><a>Navbar Item 1</a></li>
+                <li><a>Navbar Item 2</a></li>
+              </ul>
+            </div>
+          </div>
+
+          <main className="min-h-screen flex flex-row ">
+            <DashBoard />
+          </main>
+
+        </div>
+        <div className="drawer-side">
+          <label htmlFor="my-drawer-3" aria-label="close sidebar" className="drawer-overlay"></label>
+          <ul className="menu p-4 w-80 min-h-full bg-base-200">
+            {/* Sidebar content here */}
+            {/* <li><LoginButtonGoogle /></li> */}
+            <li><button className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover" >Sign out</button></li>
+          </ul>
+        </div>
       </div>
 
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
+    </main>
 
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-
-      <p>{greetMsg}</p>
-    </div>
   );
 }
 
